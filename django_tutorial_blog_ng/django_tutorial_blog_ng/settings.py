8b1714ab7db0f3dd17e75582d72ccbca147652d4
ksint <ever4everest@gmail.com>
2015-11-15 19:32:47 +0000

    Impltemented list view for posts

--- django_tutorial_blog_ng/django_tutorial_blog_ng/settings.py
+++ django_tutorial_blog_ng/django_tutorial_blog_ng/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'django_tutorial_blog_ng.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
