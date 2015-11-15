e34d4c5b94c099fb900c6f12c2a2a4f93840ceea
ksint <ever4everest@gmail.com>
2015-11-15 21:48:09 +0000

    implemented flat page support

--- django_tutorial_blog_ng/django_tutorial_blog_ng/urls.py
+++ django_tutorial_blog_ng/django_tutorial_blog_ng/urls.py
@@ -25,6 +25,10 @@ urlpatterns = patterns(
     # url(r'^$', 'django_tutorial_blog_ng.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
+
     # Blog URLs
     url(r'', include('blogengine.urls')),
+
+    # Flat pages
+    url(r'', include('django.contrib.flatpages.urls')),
 )
