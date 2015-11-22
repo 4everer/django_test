55fcb626a7e4fe303a39698bec63fad6836f649e
Guang <ever4everest@gmail.com>
2015-11-22 15:42:43 +0000

    restructured project; added Procfile; preparation of Heroku deployment;

--- django_tutorial_blog_ng/wsgi.py
+++ django_tutorial_blog_ng/wsgi.py
@@ -6,11 +6,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_tutorial_blog_ng.settings")
 
-application = get_wsgi_application()
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "django_tutorial_blog_ng.settings")
+
+application = Cling(get_wsgi_application())
